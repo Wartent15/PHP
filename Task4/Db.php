b0a1e1d10a7f077daf2b9aae33bb87f518d46156
Wartent15 <163977218+Wartent15@users.noreply.github.com>
2026-04-16 17:27:01 +0300

    Create Db.php

---
+++ Task4/Db.php
@@ -0,0 +1,15 @@
+<?php
+class Database {
+    public static function connect($host, $user, $pass, $dbname) {
+        try {
+            $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
+            return new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            die("Ошибка подключения: " . $e->getMessage());
+        }
+    }
+}
